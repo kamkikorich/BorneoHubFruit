8df6bb958eeb313698252e3d49911df926472646
kamkikorich <75824451+kamkikorich@users.noreply.github.com>
2025-11-15 16:12:31 +0800

    Create stock.js

---
+++ stock.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        "id": 1700001,
+        "name": "Buah Terap Sabah",
+        "desc": "Isi tebal, manis dan berlemak. Gred A.",
+        "price": 15,
+        "unit": "biji",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/4/46/Artocarpus_odoratissimus_scent.jpg"
+    },
+    {
+        "id": 1700002,
+        "name": "Avocado Tenom",
+        "desc": "Lemak berkrim macam mentega. Terbaik dari Tenom.",
+        "price": 20,
+        "unit": "kg",
+        "img": "https://images.unsplash.com/photo-1523049673856-386996a946f4?auto=format&fit=crop&w=500"
+    }
+];
