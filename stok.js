350c53df20b0e0e5c9babbca28b4f7972d643cc0
kamkikorich <75824451+kamkikorich@users.noreply.github.com>
2025-11-15 17:14:01 +0800

    Update Stok + Gambar

--- stok.js
+++ stok.js
@@ -1,13 +1,5 @@
 const products = [
     {
-        "id": 1710001,
-        "name": "Buah Terap",
-        "desc": "Manis Gred A",
-        "price": 15,
-        "unit": "biji",
-        "img": "https://via.placeholder.com/150"
-    },
-    {
         "id": 1710002,
         "name": "Avocado Tenom",
         "desc": "Lemak berkrim",
